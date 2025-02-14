155c827fe0e0e87291efd9460c38ea2d2cdaf3db
minahil12 <minahilmaroof5@gmail.com>
2025-02-14 21:34:55 +0500

    promisses

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+//getting data the old way using xmlhttp request
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -87,6 +89,32 @@ const getCountryDataAndNeighbour = function (country) {
   });
 };
 
-getCountryDataAndNeighbour('pakistan');
-//getCountryData('usa');
-//getCountryData('canada');
+*/
+
+const renderData = function (data, className = '') {
+  const hml = ` <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4></h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', hml);
+
+  countriesContainer.style.opacity = 1;
+};
+//Getting data using AJAX
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderData(data[0]));
+};
+
+getCountryData('pakistan');
